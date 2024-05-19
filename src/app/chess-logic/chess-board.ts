18b9a96e9c62bd957d14eb71c90f682b8fff9922
MaximKraychev <maxim.kraychev@gmail.com>
2024-05-19 10:34:16 +0300

    Add chess-board class

---
+++ src/app/chess-logic/chess-board.ts
@@ -0,0 +1,48 @@
+import { Bishop } from "./pieces/bishop.js";
+import { King } from "./pieces/king.js";
+import { Knight } from "./pieces/knight.js";
+import { Color, FENChar } from "./pieces/models.js";
+import { Pawn } from "./pieces/pawn.js";
+import { Piece } from "./pieces/piece.js";
+import { Queen } from "./pieces/queen.js";
+import { Rook } from "./pieces/rook.js";
+
+export class ChessBoard {
+    private chessBoard: (Piece | null)[][];
+    private _playerColor = Color.White;
+
+    constructor() {
+        this.chessBoard = [
+            [
+                new Rook(Color.White), new Knight(Color.White), new Bishop(Color.White), new Queen(Color.White),
+                new King(Color.White), new Bishop(Color.White), new Knight(Color.White), new Rook(Color.White)
+            ],
+            [
+                new Pawn(Color.White), new Pawn(Color.White), new Pawn(Color.White), new Pawn(Color.White),
+                new Pawn(Color.White), new Pawn(Color.White), new Pawn(Color.White), new Pawn(Color.White)
+            ],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [
+                new Pawn(Color.Black), new Pawn(Color.Black), new Pawn(Color.Black), new Pawn(Color.Black),
+                new Pawn(Color.Black), new Pawn(Color.Black), new Pawn(Color.Black), new Pawn(Color.Black)
+            ],
+            [
+                new Rook(Color.Black), new Knight(Color.Black), new Bishop(Color.Black), new Queen(Color.Black),
+                new King(Color.Black), new Bishop(Color.Black), new Knight(Color.Black), new Rook(Color.Black)
+            ]
+        ]
+    }
+
+    public get playerColor(): Color {
+        return this._playerColor;
+    }
+
+    public get chessBoardView(): (FENChar | null)[][] {
+        return this.chessBoard.map(row => {
+            return row.map(piece => piece instanceof Piece ? piece.FENChar : null);
+        })
+    }
+}
